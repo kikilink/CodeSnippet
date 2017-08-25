22493bda1e702811d03c03068504f3b1d1e1347c
kingsleylin <546324943@qq.com>
2017-08-25 22:43:57 +0800

    调整模型类的位置

--- src/main/java/kikilinux/code/snippet/ObjectUtil.java
+++ src/main/java/kikilinux/code/snippet/ObjectUtil.java
@@ -103,27 +103,4 @@ public class ObjectUtil {
 			}
 		}
 	}
-
-	public static class Student implements Serializable {
-		private String id;
-
-		private String name;
-
-		public String getName() {
-			return name;
-		}
-
-		public void setName(String name) {
-			this.name = name;
-		}
-
-		public String getId() {
-			return id;
-
-		}
-
-		public void setId(String id) {
-			this.id = id;
-		}
-	}
 }
--- src/test/java/kikilinux/code/snippet/TestObjectUitl.java
+++ src/test/java/kikilinux/code/snippet/TestObjectUitl.java
@@ -1,16 +1,41 @@
 package kikilinux.code.snippet;
 
+import java.io.Serializable;
+
 public class TestObjectUitl {
 	public static void main(String[] args) throws Throwable {
-		ObjectUtil.Student student = new ObjectUtil.Student();
+		Student student = new Student();
 		student.setId("2");
 		student.setName("kikilinux");
 
 		String hexString = ObjectUtil.writeObjectToHexString(student);
 
-		ObjectUtil.Student student2 = (ObjectUtil.Student) ObjectUtil
+		Student student2 = (Student) ObjectUtil
 				.readObjectsFromHexString(hexString);
 		System.out.println(String.format("ID:%s Name:%s", student2.getId(),
 				student2.getName()));
+	}
+
+	public static class Student implements Serializable {
+		private String id;
+
+		private String name;
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public String getId() {
+			return id;
+
+		}
+
+		public void setId(String id) {
+			this.id = id;
+		}
 	}
 }
